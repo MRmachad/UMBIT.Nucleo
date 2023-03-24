803f6c415989a425702d7c4c8ba561cb23451e54
Leonardo Machado da Silva <machadoleonardo7856@gmail.com>
2023-03-23 23:11:44 -0300

    Fist commit

---
+++ UMBIT.Nucleo/Configurate/IModuleInitializer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace UMBIT.Nucleo.Configurate
+{
+
+    public interface IModuleInitializer
+    {
+        void Init(IServiceCollection serviceCollection);
+    }
+
+}
---
+++ UMBIT.Nucleo/Configurate/LoadPlugins.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Loader;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UMBIT.Nucleo.Configurate
+{
+    public static class LoadPlugins
+    {
+        public static string PathPlugins = "UMBIT_PLUGINS";
+
+        public static void AddPluginsMVC(this IServiceCollection services, IWebHostEnvironment _hostingEnvironment)
+        {
+            var modulesInfo = CarregaPlugins(_hostingEnvironment, GetModules());
+
+            var mvcBuilder = services.AddMvc();
+
+            var moduleInitializerInterface = typeof(IModuleInitializer);
+
+            foreach (var module in modulesInfo)
+            {
+                // Registra controller dos plugins
+                mvcBuilder.AddApplicationPart(module.Assembly).AddMvcOptions(m => m.EnableEndpointRouting =true);
+
+                // Registra as depedencias dos plugins
+                var moduleInitializerType = module.Assembly.GetTypes().Where(x => typeof(IModuleInitializer).IsAssignableFrom(x)).FirstOrDefault();
+                if (moduleInitializerType != null && moduleInitializerType != typeof(IModuleInitializer))
+                {
+                    var moduleInitializer = (IModuleInitializer)Activator.CreateInstance(moduleInitializerType);
+                    moduleInitializer.Init(services);
+                }
+            }
+        }
+
+        private static IList<ModuleInfo> CarregaPlugins(IWebHostEnvironment _hostingEnvironment, IList<ModuleInfo> modules)
+        {
+
+            var binFolder = new DirectoryInfo(Environment.GetEnvironmentVariable(PathPlugins));
+            if (!binFolder.Exists)
+            {
+                return null;
+            }
+
+            foreach (var file in binFolder.GetFileSystemInfos("*.dll", SearchOption.AllDirectories))
+            {
+                Assembly assembly;
+                try
+                {
+                    assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(file.FullName);
+                }
+                catch (FileLoadException ex)
+                {
+                    if (ex.Message == "Assembly with same name is already loaded")
+                    {
+                        continue;
+                    }
+                    throw;
+                }
+
+                modules.Add(new ModuleInfo { Name = file.Name, Assembly = assembly, Path = file.FullName });
+
+            }
+
+            return modules;
+        }
+
+
+
+        private static IList<ModuleInfo> GetModules()
+        {
+            return new List<ModuleInfo>();
+        }
+    }
+
+}
